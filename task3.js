4eb5451b46195342dbc34e7bf08a4a585a48c4b7
govindbohara <np01cp4a190225@islingtoncollege.edu.np>
2022-05-05 18:25:37 +0545

    changed js

--- task3.js
+++ task3.js
@@ -1,21 +1,22 @@
-const calculateShortestTime = (n, m, a, b, c, d, _case, travelMethod = 'Elevator') => {
-	_case && console.log(`=================${_case}==============`);
+const calculateShortestTime = (n, m, a, b, c, d, travelMethod = 'Elevator') => {
 	const floorDiff = Math.abs(m - n);
 	if (travelMethod === 'Walking') {
 		const totalTimeForWalk = n === 0 ? 0 : n * d;
-		return console.log(`Total time for walking : ${totalTimeForWalk} Seconds`);
+
+		return totalTimeForWalk;
 	}
 	if (travelMethod === 'Elevator') {
 		const totalTimeForElevator = n === 0 ? 0 : floorDiff * a + b + c + a * n + b;
-		return console.log(`Total time for elevator : ${totalTimeForElevator} seconds`);
+
+		return totalTimeForElevator;
 	}
 	const timeForBoth = d + b + c + m * a + b;
-	console.log(`Time for both walk and elevator combined : ${timeForBoth}`);
+
+	return timeForBoth;
 };
-calculateShortestTime(4, 5, 1, 2, 3, 10, 'Case 1');
-calculateShortestTime(0, 5, 1, 2, 3, 10, 'Case 2');
-calculateShortestTime(4, 3, 2, 3, 4, 5, 'Case 3', 'Walking');
-calculateShortestTime(7, 6, 3, 1, 1, 4, 'Case 4', 'Elevator');
-calculateShortestTime(7, 6, 3, 1, 1, 4, 'Case 5', 'Both');
-calculateShortestTime(7, 6, 3, 1, 1, 4, 'Case 6', 'Walking');
-calculateShortestTime(7, 6, 3, 1, 1, 4, 'Case 6', 'Elevator');
+
+const timeForWalk = calculateShortestTime(4, 3, 2, 3, 4, 5, 'Walking');
+const timeForElevator = calculateShortestTime(4, 3, 2, 3, 4, 5, 'Elevator');
+const timeForBoth = calculateShortestTime(4, 3, 2, 3, 4, 5, 'Both');
+const shortestDistance = Math.min(timeForWalk, timeForElevator, timeForBoth);
+console.log(`The shortest time is ${shortestDistance} seconds`);
